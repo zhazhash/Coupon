5c54eef49e4cdefaf624af3ff22c7047ffa2e3aa
admin <465634154@qq.com>
2022-01-07 22:44:23 +0800

    优惠卷执行规则管理器 与结算controller

---
+++ sh-coupon-service/coupon-settlement/src/main/java/com/sh/coupon/controller/SettlementController.java
@@ -0,0 +1,33 @@
+package com.sh.coupon.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.sh.coupon.ecxeption.CouponException;
+import com.sh.coupon.executor.ExecutorManger;
+import com.sh.coupon.vo.SettlementInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.codec.CodecException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SettlementController {
+    /*结算规则执行管理器*/
+    private final ExecutorManger executorManger;
+
+    public SettlementController(ExecutorManger executorManger) {
+        this.executorManger = executorManger;
+    }
+
+    /**
+     * 优惠卷结算
+     * @param settlementInfo
+     * @return
+     * @throws CodecException
+     */
+    @PostMapping("/sellement/computerule")
+    public SettlementInfo computeRule(SettlementInfo settlementInfo) throws CouponException {
+        log.info("settlement:{}", JSON.toJSONString(settlementInfo));
+        return executorManger.couputeRule(settlementInfo);
+    }
+}
---
+++ sh-coupon-service/coupon-settlement/src/main/java/com/sh/coupon/executor/ExecutorManger.java
@@ -0,0 +1,74 @@
+package com.sh.coupon.executor;
+
+import com.sh.coupon.constant.CouponCategory;
+import com.sh.coupon.constant.RuleFlag;
+import com.sh.coupon.ecxeption.CouponException;
+import com.sh.coupon.vo.SettlementInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 优惠卷执行规则管理器
+ * 即根据用户的请求找到对应的Executor ，去做结算
+ * BeanPostProcessor ： bean 后置处理器
+ *
+ */
+@Slf4j
+@Component
+public class ExecutorManger implements BeanPostProcessor {
+
+    private Map<RuleFlag,RuleExecutor> executorIndex = new HashMap<>();
+
+    /**
+     * 优惠卷规则计算入口 ，需保证传递的优惠卷个数大于等于1
+     * @param settlementInfo
+     * @return
+     * @throws CouponException
+     */
+    public SettlementInfo couputeRule (SettlementInfo settlementInfo) throws CouponException{
+        SettlementInfo result = null;
+        if(settlementInfo.getCouponAndTemplateInfos().size() == 1){
+            CouponCategory category = CouponCategory.of(settlementInfo.getCouponAndTemplateInfos().get(0)
+                    .getCouponTemplate().getCategory());
+            return executorIndex.get(category).couputeRule(settlementInfo);
+
+        }
+        if(settlementInfo.getCouponAndTemplateInfos().size() == 2){
+            List<CouponCategory> categories = new ArrayList<>();
+            settlementInfo.getCouponAndTemplateInfos().forEach(ct ->
+                    categories.add(CouponCategory.of(ct.getCouponTemplate().getCategory())));
+            if (categories.contains(RuleFlag.MANJIAN) && categories.contains(RuleFlag.ZHEKOU)){
+               return executorIndex.get(RuleFlag.MANJIAN_ZHEKOU).couputeRule(settlementInfo);
+            }
+        }
+        throw new CouponException("Not Support For More Template Category");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(!(bean instanceof RuleExecutor)){
+            return bean;
+        }
+        RuleExecutor ruleExecutor = (RuleExecutor)bean;
+        if (executorIndex.containsKey(ruleExecutor.ruleConfig())){
+            throw new IllegalStateException("There is alreadly an Executor for rule" +
+                    "flag" + ruleExecutor.ruleConfig().toString());
+        }
+        log.debug("Load executor {} for rule flag {}" ,ruleExecutor.getClass(),ruleExecutor.ruleConfig());
+        executorIndex.put(ruleExecutor.ruleConfig(),ruleExecutor);
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+}
